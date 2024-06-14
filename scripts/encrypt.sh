dcc30c3efb2fbab2a1207db1b7f7f06d96d31a75
BarryChum <barrychum@users.noreply.github.com>
2024-06-14 08:04:27 +0100

    Update encrypt.sh

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -8,11 +8,25 @@ fi
 
 # Define the input and output files
 INPUT_FILE=$1
-ZIP_FILE="${INPUT_FILE}.zip"
-ENCRYPTED_FILE="${ZIP_FILE}.enc"
+RANDOM_SUFFIX=$(uuidgen | tr -d '-')
+ZIP_FILE="${INPUT_FILE}.zip.${RANDOM_SUFFIX}"
+ENCRYPTED_FILE="${INPUT_FILE}.enc.${RANDOM_SUFFIX}"
 
 # Path to the public key
 PUBLIC_KEY_PATH="$HOME/.ssh/encryption.pub"
+
+if [ ! -f "$INPUT_FILE" ]; then
+  echo "Error: Input file '$INPUT_FILE' not found."
+  exit 1
+fi
+if [ ! -f "$PUBLIC_KEY_PATH" ]; then
+  echo "Error: Public key file '$PUBLIC_KEY_PATH' not found."
+  exit 1
+fi
+if [ -f "${INPUT_FILE}.bundle" ]; then
+  echo "Error: Output file '${INPUT_FILE}.bundle' already exists."
+  exit 1
+fi
 
 # Zip the input file
 zip -q $ZIP_FILE $INPUT_FILE
@@ -27,7 +41,7 @@ openssl enc -aes-256-cbc -salt -pbkdf2 -in $ZIP_FILE -out $ENCRYPTED_FILE -pass 
 ENC_SYM_KEY=$(echo -n $SYM_KEY | openssl pkeyutl -encrypt -pubin -inkey $PUBLIC_KEY_PATH | xxd -p)
 
 # Combine the encrypted symmetric key and the encrypted file
-cat <(echo "$ENC_SYM_KEY" | xxd -r -p) $ENCRYPTED_FILE > "${INPUT_FILE}.bundle"
+cat <(echo "$ENC_SYM_KEY" | xxd -r -p) $ENCRYPTED_FILE >"${INPUT_FILE}.bundle"
 
 # Clean up
 rm $ZIP_FILE $ENCRYPTED_FILE
