3fdd5576d6091cc0b6d38e05f59b6d903b02b4cd
BarryChum <barrychum@users.noreply.github.com>
2024-06-14 07:29:43 +0100

    Update encrypt.sh

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -10,7 +10,6 @@ fi
 INPUT_FILE=$1
 ZIP_FILE="${INPUT_FILE}.zip"
 ENCRYPTED_FILE="${ZIP_FILE}.enc"
-ENC_SYM_KEY_FILE="symmetric_key.bin.enc"
 
 # Path to the public key
 PUBLIC_KEY_PATH="$HOME/.ssh/encryption.pub"
@@ -25,12 +24,16 @@ SYM_KEY=$(openssl rand -base64 32)
 openssl enc -aes-256-cbc -salt -pbkdf2 -in $ZIP_FILE -out $ENCRYPTED_FILE -pass pass:$SYM_KEY
 
 # Encrypt the symmetric key using the public key with pkeyutl
-echo -n $SYM_KEY | openssl pkeyutl -encrypt -inkey $PUBLIC_KEY_PATH -pubin -out $ENC_SYM_KEY_FILE
+ENC_SYM_KEY=$(echo -n $SYM_KEY | openssl pkeyutl -encrypt -pubin -inkey $PUBLIC_KEY_PATH | xxd -p)
 
 # Combine the encrypted symmetric key and the encrypted file
-cat $ENC_SYM_KEY_FILE $ENCRYPTED_FILE > "${INPUT_FILE}.bundle"
+cat <(echo "$ENC_SYM_KEY" | xxd -r -p) $ENCRYPTED_FILE > "${INPUT_FILE}.bundle"
+# {
+#  echo "$ENC_SYM_KEY" | xxd -r -p
+#  cat $ENCRYPTED_FILE
+# } > "${INPUT_FILE}.bundle"
 
 # Clean up
-rm $ZIP_FILE $ENC_SYM_KEY_FILE $ENCRYPTED_FILE
+rm $ZIP_FILE $ENCRYPTED_FILE
 
 echo "File has been encrypted and stored as ${INPUT_FILE}.bundle"
